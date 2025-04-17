5cf4a92153452abe323fddf18fe52974449a699b
eangelinelli <eangelinelli@tai.it>
2025-04-17 13:54:55 +0200

    initial

---
+++ src/main/java/org/acme/statistics/HeroStatistics.java
@@ -0,0 +1,24 @@
+package org.acme.statistics;
+
+import io.quarkus.funqy.Funq;
+import io.smallrye.graphql.client.GraphQLClient;
+import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
+import org.acme.statistics.hero.GraphQLHeroClient;
+
+import java.time.Instant;
+
+public class HeroStatistics {
+
+    @Inject
+    @GraphQLClient("inventory")
+    GraphQLHeroClient inventoryClient;
+
+    @Funq
+    public Uni<String> getHeroStatistics() {
+        return inventoryClient.allHeroes()
+                .map(cars -> ("The Heroes statistics created at %s. " +
+                        "Number of available heroes: %d")
+                        .formatted(Instant.now(), cars.size()));
+    }
+}
